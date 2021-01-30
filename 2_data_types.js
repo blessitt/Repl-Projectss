9d3d41b8fb7c76902a68b472ac8595bb09dead70
repl.it user <replituser@example.com>
2021-01-30 00:59:15 +0000

    Added two concepts

---
+++ 2_data_types.js
@@ -0,0 +1,35 @@
+let variable ='Carter'; 
+variable = 15; 
+variable = false; 
+variable = undefined;  
+variable = Symbol('My new symbol'); 
+
+variable = 1; 
+variable = 10.2030; 
+variable = -300; 
+variable = 20; 
+variable = '20';
+
+//booleans 
+variable = false; 
+//null 
+let bankSavings = null; 
+//undefined 
+let person ;  
+// Symbols 
+let symbolCS5 = Symbol('This is a Symbol'); 
+// Arrays 
+let languages = ['HTML5', 'JS', 'PHP']; 
+// Objects 
+let person2 = { 
+  name: 'Carter', 
+  country: 'United States'
+} 
+// Dates 
+let today = new Date ();
+
+
+
+console.log(typeof today);
+
+ 
