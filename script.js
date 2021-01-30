5392d6a96b8d710b9a07bc819a7436456f369261
blessitt <carter.craney@hgs.hiddengeniusproject.org>
2021-01-30 00:16:57 +0000

    I added the code for the second variable video(Variables with let)

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-var learning = 'Javascript'; 
-var age = '99';
+//let & const
+let learning = 'Modern Javascript'; 
+var age = '15';
 console.log(age); 
 
-var days99 =99;
+var days99 =15; 
 
+let name = 'Carter';
+console.log(name);  
 
+let product1 = 'Book', 
+product2 = 'Shirt'; 
+console.log(product1);
+console.log(product2);
+ 
+ let shoppingCart = ['Product1', 'Product2', 'Product3']; 
+ console.log(shoppingCart);
