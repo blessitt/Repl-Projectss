f906866b5ab460823c8835a8eb8407726c755878
blessitt <carter.craney@hgs.hiddengeniusproject.org>
2021-02-13 22:37:45 +0000

    I added the string code

---
+++ 3_strings.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+let name;  
+console.log("hey");
+//Quotes  
+
+ name = 'Carter Smith';  
+ name = "Carter Smith"; 
+ let message = "Then I said: hey!! that\'s nice";
+
+ console.log(message);
